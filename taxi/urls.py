bbfd81edbfc70e6648eeba36401a58776cc0621e
Kyryl <sautakiki.com@gmail.com>
2026-04-02 23:59:37 +0300

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
 from taxi.views import index
 
 
@@ -8,6 +9,6 @@ less_words = settings.STATIC_ROOT
 urlpatterns = ([path("",
                      index,
                      name="index")
-                ] + static(settings.STATIC_URL, document_root=less_words))
+                ])
 
 app_name = "taxi"
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -20,5 +20,3 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("taxi.urls", "taxi"), namespace="taxi"))
 ]
-
-app_name = "taxi"
